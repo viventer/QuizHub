c70bf61619d3803c51a3659eb2cadbb386f6eab9
Oskar Goczał <viventermail@gmail.com>
2023-04-03 21:44:27 +0200

    poprawki dla bardzo wąskich ekranów

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const textLogo = document.querySelector(".text-logo");
 const searchBar = document.querySelector(".search-bar");
+const searchIconBox = document.querySelector(".search-icon-box");
+const signInButton = document.querySelector(".sign-in-button");
 
 searchBar.addEventListener("focus", () => {
   textLogo.style.display = "none";
@@ -7,4 +9,17 @@ searchBar.addEventListener("focus", () => {
 
 searchBar.addEventListener("blur", () => {
   textLogo.style.display = "block";
+  if (window.innerWidth < 350) {
+    searchBar.style.display = "none";
+  }
 });
+
+searchIconBox.addEventListener("click", () => {
+  searchBar.style.display = "block";
+  searchBar.focus();
+});
+
+if (window.innerWidth < 305) {
+  signInButton.innerHTML = '<i class="icon-user"></i>';
+  signInButton.style.padding = "0 0.1rem";
+}
