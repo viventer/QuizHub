09e9a9040d4b82d7233547e95d5de99367828a70
Oskar Goczał <viventermail@gmail.com>
2023-04-07 16:29:41 +0200

    dokończenie quiz slidera i poprawa headera

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 const textLogo = document.querySelector(".text-logo");
 const searchBar = document.querySelector(".search-bar");
 const searchIconBox = document.querySelector(".search-icon-box");
-const signInButton = document.querySelector(".sign-in-button");
+const loginButton = document.querySelector(".login-button");
 
-searchBar.addEventListener("focus", () => {
-  textLogo.style.display = "none";
-});
+if (window.innerWidth < 500) {
+  searchBar.addEventListener("focus", () => {
+    textLogo.style.display = "none";
+  });
 
-searchBar.addEventListener("blur", () => {
-  textLogo.style.display = "block";
-  if (window.innerWidth < 350) {
-    searchBar.style.display = "none";
+  searchBar.addEventListener("blur", () => {
+    textLogo.style.display = "block";
+    if (window.innerWidth < 350) {
+      searchBar.style.display = "none";
+    }
+  });
+
+  searchIconBox.addEventListener("click", () => {
+    searchBar.style.display = "block";
+    searchBar.focus();
+  });
+
+  if (window.innerWidth < 315) {
+    loginButton.innerHTML = '<i class="icon-user"></i>';
+    loginButton.style.padding = "0 0.1rem";
   }
-});
-
-searchIconBox.addEventListener("click", () => {
-  searchBar.style.display = "block";
-  searchBar.focus();
-});
-
-if (window.innerWidth < 315) {
-  signInButton.innerHTML = '<i class="icon-user"></i>';
-  signInButton.style.padding = "0 0.1rem";
 }
